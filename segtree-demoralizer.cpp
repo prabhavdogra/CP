381bd7d2282091a3cd79fc605a5c73090939ca3e
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-02-14 16:05:39 +0530

    Update segtree-demoralizer.cpp

--- segtree-demoralizer.cpp
+++ segtree-demoralizer.cpp
@@ -1,175 +1,245 @@
 #include <bits/stdc++.h>
-using namespace std;
 #define ll long long int
+using namespace std;
 
+const int32_t MAXN = 1e5;
 
-template<typename node,typename update>
-struct segtree
-{
-	ll leng;
-	vector<node> t;
-	vector<update> u;
-	vector<bool> lazy;
-	node identity_element;
-	update identity_transformation;
-	segtree(ll l){
-		leng = l;
-		t.resize(4 * leng);
-		u.resize(4 * leng);
-		lazy.resize(4 * leng);
-		identity_element = node();
-		identity_transformation = update();
-	}
-	
-	void pushdown(const int32_t &v, const int32_t &tl, const int32_t &tr){
-		if(!lazy[v]) return;
-		int32_t tm = (tl + tr) >> 1;
-		apply(v<<1,tl,tm,u[v]);
-		apply(v<<1|1,tm+1,tr,u[v]);
-		u[v] = identity_transformation;
-		lazy[v] = 0;
-	}
-	
-	void apply(const int32_t &v, const int32_t &tl, const int32_t &tr, update upd){
-		if(tl != tr){
-			lazy[v] = 1;
-			u[v].combine(upd,tl,tr);
+template <typename NODE, typename UPDATE>
+struct segtree {
+	bool built = false, lazy[4 * MAXN];
+	NODE zero = NODE(), t[4 * MAXN];
+	UPDATE noop = UPDATE(), upds[4 * MAXN];
+	int32_t tl[4 * MAXN], tr[4 * MAXN];
+	inline void pushdown(int32_t v)	{
+		if (lazy[v]) {
+			apply(v * 2, upds[v]);
+			apply(v * 2 + 1, upds[v]);
+			lazy[v] = 0;
+			upds[v] = noop;
 		}
-		upd.apply(t[v],tl,tr);
 	}
-	
-	template<typename T>
-	void build(const T &arr,const int32_t &v, const int32_t &tl, const int32_t &tr){
-		if(tl == tr){
-			t[v] = arr[tl];
+	inline void apply(int32_t v, UPDATE &val) {
+		if (tl[v] != tr[v]) {
+			lazy[v] = 1;
+			upds[v].combine(val, tl[v], tr[v]);
+		}
+		val.apply(t[v], tl[v], tr[v]);
+	}
+	template <typename T>
+	void build(T &a, int32_t v, int32_t l, int32_t r) {
+		tl[v] = l;
+		tr[v] = r;
+		if (l == r) {
+			t[v] = a[l];
 			return;
 		}
-		int32_t tm = (tl + tr) >> 1;
-		build(arr,v<<1,tl,tm);
-		build(arr,v<<1|1,tm+1,tr);
-		t[v].merge(t[v<<1],t[v<<1|1]);
-	}
-	
-	node query(const int32_t &v,const int32_t &tl,const int32_t &tr,const int32_t &l,const int32_t &r){
-		if(l > tr || r < tl){
-			return identity_element;
+		else {
+			int32_t tm = (l + r) / 2;
+			build(a, v * 2, l, tm);
+			build(a, v * 2 + 1, tm + 1, r);
+			t[v].merge(t[v * 2], t[v * 2 + 1]);
 		}
-		if(tl >= l && tr <= r){
+	}
+	NODE query(int32_t v, ll l, ll r) {
+		if (l > tr[v] || r < tl[v])
+			return zero;
+		if (l <= tl[v] && tr[v] <= r) {
 			return t[v];
 		}
-		pushdown(v,tl,tr);
-		int32_t tm = (tl + tr) >> 1;
-		node a = query(v<<1,tl,tm,l,r),b = query(v<<1|1,tm+1,tr,l,r),ans;
-		ans.merge(a,b);
+		pushdown(v);
+		NODE a, b, ans;
+		a = query(v * 2, l, r);
+		b = query(v * 2 + 1, l, r);
+		ans.merge(a, b);
 		return ans;
 	}
-	
-	// rupd = range update
-	void rupd(const int32_t &v,const int32_t &tl,const int32_t &tr,const int32_t &l,const int32_t &r,const update &upd){
-		if(l > tr || r < tl){
+	void rupd(int32_t v, ll l, ll r, UPDATE &val) {
+		if (l > tr[v] || r < tl[v])
+			return;
+		if (l <= tl[v] && tr[v] <= r) {
+			apply(v, val);
 			return;
 		}
-		if(tl >= l && tr <= r){
-			apply(v,tl,tr,upd);
-			return;
+		pushdown(v);
+		rupd(v * 2, l, r, val);
+		rupd(v * 2 + 1, l, r, val);
+		t[v].merge(t[v * 2], t[v * 2 + 1]);
+	}
+	template <typename T>
+	ll descent_right(ll l, T x, int32_t v, NODE &prev) {
+		if (l > tr[v]) // No overlap
+			return leng;
+		if (l <= tl[v])	{ // Complete overlap
+			NODE cur;
+			cur.merge(prev, t[v]);
+			if (!cur.check(x)) { // Going further right than current node
+				swap(prev, cur); // Including node's contribution
+				return leng;
+			}
+			if (tl[v] == tr[v]) {
+				return tr[v];
+			}
 		}
-		pushdown(v,tl,tr);
-		int32_t tm = (tl + tr) >> 1;
-		rupd(v<<1,tl,tm,l,r,upd);
-		rupd(v<<1|1,tm+1,tr,l,r,upd);
-		t[v].merge(t[v<<1],t[v<<1|1]);
+		pushdown(v);
+		ll ans = descent_right(l, x, v * 2, prev); // Finding in left child
+		if (ans != leng) // Found in left child
+			return ans;								 
+		return descent_right(l, x, v * 2 + 1, prev); // Finding in right child
 	}
-	
-	public:
-	template<typename T>
-	void build(const T &arr){
-		build(arr,1,0,leng-1);
+	template <typename T>
+	ll descent_left(ll r, T x, int32_t v, NODE &prev)
+	{
+		if (r < tl[v]) // No overlap
+			return -1;
+		if (r >= tr[v])	{ // Complete overlap
+			NODE cur;
+			cur.merge(t[v], prev);
+			if (!cur.check(x)) { // Going further left than current node
+				swap(cur, prev); // Including node's contribution
+				return -1;
+			}
+			if (tl[v] == tr[v])	{
+				return tl[v];
+			}
+		}
+		pushdown(v);
+		ll ans = descent_left(r, x, v * 2 + 1, prev); // Finding in right child
+		if (ans != -1) // Found in right child
+			return ans;
+		return descent_left(r, x, v * 2, prev); // Finding in left child
 	}
-	node query(const int32_t &l,const int32_t &r){
-		return query(1,0,leng-1,l,r);
+
+	ll leng = MAXN;
+	void clear() {
+		fill(t, t + 4 * leng, zero);
+		fill(lazy, lazy + 4 * leng, false);
+		fill(upds, upds + 4 * leng, noop);
+		built = false;
 	}
-	void rupd(const int32_t &l,const int32_t &r,const update &upd){
-		rupd(1,0,leng-1,l,r,upd);
+	template <typename T>
+	void build(T &a) {
+		build(a, 1, 0, leng - 1);
+		built = true;
+	}
+	template <typename T>
+	ll descent_right(ll l, T x)	{ 
+		// Moves left to right, returns segtree.leng if not found
+		NODE prev = zero;
+		return descent_right(l, x, 1, prev);
+	}
+	template <typename T>
+	ll descent_left(ll r, T x) { 
+		// Moves right to left, returns -1 if not found
+		NODE prev = zero;
+		return descent_left(r, x, 1, prev);
+	}
+	NODE query(ll l, ll r) {
+		if (!built)
+			build(t);
+		return query(1, l, r);
+	}
+	void rupd(ll l, ll r, UPDATE val) {
+		if (!built)
+			build(t);
+		rupd(1, l, r, val);
 	}
 };
+
+#define node node1
+#define update update1
 /*
 KEYNOTES:
-1. merge(X,identity_element) = X (for nodes other than leaf node)
-2. identity_transformation.combine(X) = X (combining Identity element with update)
-3. NOTE: older_update.combine(newer_update) (how updates will happen)
+    1. merge(X,identity_element) = X (for nodes other than leaf node)
+    2. identity_transformation.combine(X) = X (combining identity transformation with update)
+    3. NOTE: older_update.combine(newer_update) (how updates will happen)
 */
 
 // Example: In Addition the identity element is 0, 
 //          i.e., -> a + 0 = a or 0 + a = a
 // 			Also: min(x,INT_MAX) = x
 
+
 // Structure of each node in the segment tree
-struct node
-{
+struct node {
 	// Initialised with the identity element
 	ll v = 0;
 	// Add more variables if needed
-	node() {} 
+	node() {}
 	node(ll val) { // Building the tree
 		v = val;
 	}
 	// Merging l and r nodes for each segment
-	void merge(const node &l,const node &r) { 
+	void merge(node &l, node &r) {
 		v = l.v + r.v;
 		// For maximum we'll do
-		// v = max(l.v, r.v);
+        // v = max(l.v, r.v);
+	}
+	bool check(ll x) { // Gradient descent
+		if (v >= x)
+			return true;
+		return false;
 	}
 };
 
 // Example: In Addition the identity transformation is 0, 
-//          i.e., -> old += new + 0;
+//          i.e., > old_update = new_update + 0;
 
-struct update
-{
+struct update {
 	// Initialised with the identity transformation
 	ll v = 0;
 	// Add more variables if needed
 	update() {}
-	update(ll val){
+	update(ll val) {
 		v = val;
 	}
-	// Updating a node which contains identity element
-	void combine(update &other,const int32_t &tl,const int32_t &tr){
+	// Combining old update with new updates
+	void combine(update &other, int32_t tl, int32_t tr) {
 		v += other.v;
 		// only required for range updates
 	}
-	// Updating a general node
-	void apply(node &x, const int32_t &tl, const int32_t &tr) { 
+	// Applying lazy values on nodes
+	void apply(node &x, int32_t tl, int32_t tr)	{
 		x.v += v;
 	}
 };
 
+segtree<node, update> s;
 
-
-// segtree<node,update> s(100005);
-// vector<int> v(100005); // take input in the vector
-// s.build(v);
+#undef node
+#undef update
 
 // Single element: s.rupd(index, index, value);
 // Range Update: s.rupd(start, end, value);
 // Query: s.query(start, end).v
+// Descent: s.descent_right(index, value); -> Left to right
+//       -> Returns segtree.leng if not found
+//          s.descent_left(index, value);  -> Right tp left
+//       -> Returns -1 if not found
 
 // NOTE: All indexing is 0 based
 
-
-int32_t main() {
-	int n;
+int main() 
+{   
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin) ;
+		freopen("output.txt", "w", stdout) ;
+	#endif 
+	ll n;
 	cin >> n;
-	segtree<node,update> s(n);
-	vector<ll> v(n);
+	s.leng = n;
+	s.clear();
+	vector<ll> v(n);	
 	for(ll i = 0 ; i < n ; i++)
-		v[i] = {0};
+		cin >> v[i];
 	s.build(v);
-	
 	// Single element: s.rupd(index, index, value);
 	// Range Update: s.rupd(start, end, value);
 	// Query: s.query(start, end).v
-	
+	// Descent: s.descent_right(index, value); -> Left to right
+	//       -> Returns segtree.leng if not found
+	//          s.descent_left(index, value);  -> Right tp left
+	//       -> Returns -1 if not found
 	return 0;
 }
-
+ 
