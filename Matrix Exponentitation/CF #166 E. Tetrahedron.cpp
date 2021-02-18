4d1afd38a890f22f9c792c153acad9334fb6cfa2
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-02-18 15:08:55 +0530

    Update CF #166 E. Tetrahedron.cpp

--- Matrix Exponentitation/CF #166 E. Tetrahedron.cpp
+++ Matrix Exponentitation/CF #166 E. Tetrahedron.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
-#define ll long long int
+#define ll 	long long int
+#define M 	100000007
 using namespace std; 
 
-// B. Decoding Genome
-// https://codeforces.com/problemset/problem/222/E
+// E. Tetrahedron
+// https://codeforces.com/problemset/problem/166/E
 
 ll mod(ll x) {
-	if(x < 0) x += 1000000007;
-	return x % 1000000007;
+	if(x < 0) x += M;
+	return x % M;
 }
 
 vector <vector<ll>> matmul(vector<vector<ll>> &a, vector<vector<ll>> &b) {
@@ -38,35 +39,26 @@ vector<vector<ll>> matexp(vector<vector<ll>> m, ll n) {
 	return a;
 }
 
-void pr(vector<vector<ll>> a) {
-	for(ll i = 0 ; i < a.size() ; i++) { 
-		for(ll j = 0 ; j < a[0].size() ; j++) 
-			cout << a[i][j] << " "; 
-		cout << endl;
-	}
-}
-
 int main() { 
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	ll n, m, k;
-	cin >> n >> m >> k;
-	vector<vector<ll>> a(m, vector<ll>(m, 1));
-	for(ll i = 0 ; i < k ; i++) {
-		char c1, c2;
-		cin >> c1 >> c2;
-		ll x, y;
-		if(c1 <= 'Z' && c1 >= 'A') x = c1 - 'A' + 26;
-		else x = c1 - 'a';
-		if(c2 <= 'Z' && c2 >= 'A') y = c2 - 'A' + 26;
-		else y = c2 - 'a';
-		a[x][y] = 0;
-	}
-	vector <vector<ll>> c = matexp(a, n - 1);
-	pr(c);
-	ll ans = 0;
-	for(ll i = 0 ; i < c.size() ; i++) 
-		for(ll j = 0 ; j < c[0].size() ; j++) 
-			ans = mod(ans + c[i][j]);
-	cout << mod(ans) << endl;
+	ll n;
+	cin >> n;
+	vector<vector<ll>> a = 
+	{
+		{0},
+		{0},
+		{0},
+		{1}
+	};
+	vector<vector<ll>> b = 
+	{
+		{0, 1, 1, 1},
+		{1, 0, 1, 1},
+		{1, 1, 0, 1},
+		{1, 1, 1, 0}
+	};	
+	vector<vector<ll>> c = matexp(b, n);
+	vector<vector<ll>> d = matmul(c, a);
+	cout << d[3][0];
 	return 0;
 }
