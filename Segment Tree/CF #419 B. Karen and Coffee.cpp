7e1a21702e5eb0f7608f8db51d402e5ed1d6840b
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-02-25 16:00:57 +0530

    Add files via upload

---
+++ Segment Tree/CF #419 B. Karen and Coffee.cpp
@@ -0,0 +1,218 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll              long long int
+
+// B. Karen and Coffee
+// https://codeforces.com/problemset/problem/816/B
+
+const int32_t SZ = 2e5 + 5;
+ll k;
+
+template <typename NODE, typename UPDATE>
+struct segtree {
+	bool built = false, lazy[4 * SZ];
+	NODE zero = NODE(), t[4 * SZ];
+	UPDATE noop = UPDATE(), upds[4 * SZ];
+	int32_t tl[4 * SZ], tr[4 * SZ];
+	inline void pushdown(int32_t v)	{
+		if (lazy[v]) {
+			apply(v * 2, upds[v]);
+			apply(v * 2 + 1, upds[v]);
+			lazy[v] = 0;
+			upds[v] = noop;
+		}
+	}
+	inline void apply(int32_t v, UPDATE &val) {
+		if (tl[v] != tr[v]) {
+			lazy[v] = 1;
+			upds[v].combine(val, tl[v], tr[v]);
+		}
+		val.apply(t[v], tl[v], tr[v]);
+	}
+	template <typename T>
+	void build(T &a, int32_t v, int32_t l, int32_t r) {
+		tl[v] = l;
+		tr[v] = r;
+		if (l == r) {
+			t[v] = a[l];
+			if(t[v].v >= k) { //**NOTE: Modified this to store count
+				t[v].cnt = 1; // of elements >= k
+			}
+			return;
+		}
+		else {
+			int32_t tm = (l + r) / 2;
+			build(a, v * 2, l, tm);
+			build(a, v * 2 + 1, tm + 1, r);
+			t[v].merge(t[v * 2], t[v * 2 + 1]);
+		}
+	}
+	NODE query(int32_t v, ll l, ll r) {
+		if (l > tr[v] || r < tl[v])
+			return zero;
+		if (l <= tl[v] && tr[v] <= r) {
+			return t[v];
+		}
+		pushdown(v);
+		NODE a, b, ans;
+		a = query(v * 2, l, r);
+		b = query(v * 2 + 1, l, r);
+		ans.merge(a, b);
+		return ans;
+	}
+	void rupd(int32_t v, ll l, ll r, UPDATE &val) {
+		if (l > tr[v] || r < tl[v])
+			return;
+		if (l <= tl[v] && tr[v] <= r) {
+			apply(v, val);
+			return;
+		}
+		pushdown(v);
+		rupd(v * 2, l, r, val);
+		rupd(v * 2 + 1, l, r, val);
+		t[v].merge(t[v * 2], t[v * 2 + 1]);
+	}
+	template <typename T>
+	ll descent_right(ll l, T x, int32_t v, NODE &prev) {
+		if (l > tr[v]) // No overlap
+			return leng;
+		if (l <= tl[v])	{ // Complete overlap
+			NODE cur;
+			cur.merge(prev, t[v]);
+			if (!cur.check(x)) { // Going further right than current node
+				swap(prev, cur); // Including this node's contribution
+				return leng;
+			}
+			if (tl[v] == tr[v]) {
+				return tr[v];
+			}
+		}
+		pushdown(v);
+		ll ans = descent_right(l, x, v * 2, prev); // Finding in left child
+		if (ans != leng) // Found in left child
+			return ans;
+		return descent_right(l, x, v * 2 + 1, prev); // Finding in right child
+	}
+	template <typename T>
+	ll descent_left(ll r, T x, int32_t v, NODE &prev) {
+		if (r < tl[v]) // No overlap
+			return -1;
+		if (r >= tr[v]) { // Complete overlap
+			NODE cur;
+			cur.merge(t[v], prev);
+			if (!cur.check(x)) { // Going further right than current node
+				swap(cur, prev); // Including node's contribution
+				return -1;
+			}
+			if (tl[v] == tr[v]) {
+				return tl[v];
+			}
+		}
+		pushdown(v);
+		ll ans = descent_left(r, x, v * 2 + 1, prev); // Finding in right child
+		if (ans != -1) // Found in right child
+			return ans;
+		return descent_left(r, x, v * 2, prev); // Finding in left child
+	}
+
+	ll leng = SZ;
+	void clear() {
+		fill(t, t + 4 * leng, zero);
+		fill(lazy, lazy + 4 * leng, false);
+		fill(upds, upds + 4 * leng, noop);
+		built = false;
+	}
+	template <typename T>
+	void build(T &a) {
+		build(a, 1, 0, leng - 1);
+		built = true;
+	}
+	template <typename T>
+	ll descent_right(ll l, T x) { 
+		// Moves left to right, returns segtree.leng if not found
+		NODE prev = zero;
+		return descent_right(l, x, 1, prev);
+	}
+	template <typename T>
+	ll descent_left(ll r, T x) { 
+		// Moves right to left, returns -1 if not found
+		NODE prev = zero;
+		return descent_left(r, x, 1, prev);
+	}
+	NODE query(ll l, ll r) {
+		if (!built)
+			build(t);
+		return query(1, l, r);
+	}
+	void rupd(ll l, ll r, UPDATE val) {
+		if (!built)
+			build(t);
+		rupd(1, l, r, val);
+	}
+};
+
+#define node node1
+#define update update1
+
+struct node {
+	ll v = 0, cnt = 0;
+	node() {}
+	node(ll val) { 
+		v = val;
+	}
+	void merge(node &l, node &r) {
+		v = l.v + r.v;
+		cnt = l.cnt + r.cnt;
+	}
+	bool check(ll x) {
+		return false;
+	}
+};
+
+struct update {
+	update() {}
+	update(ll val) {
+	}
+	void combine(update &other, int32_t tl, int32_t tr)	{
+	}
+	void apply(node &x, int32_t tl, int32_t tr)	{
+	}
+};
+
+segtree<node, update> s;
+
+#undef node
+#undef update
+
+const ll MAXN = 2e5 + 5;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	#ifndef ONLINE_JUDGE
+		// freopen("input.txt", "r", stdin) ;
+		// freopen("output.txt", "w", stdout) ;
+	#endif 
+	ll n, q, temp, ans = 0;
+	cin >> n >> k >> q;
+	vector<ll> a(MAXN, 0);
+	for(ll i = 0 ; i < n ; i++) { // Difference Array
+		ll l, r;
+		cin >> l >> r;
+		a[l]++;
+		a[r + 1]--;
+	}
+	vector<ll> p(MAXN, 0);
+	p[0] = a[0];
+	for(ll i = 1 ; i < MAXN ; i++) {
+		p[i] = a[i] + p[i - 1];
+	}
+	s.leng = MAXN;
+	s.clear();
+	s.build(p);
+	for(ll i = 0 ; i < q ; i++) {
+		ll a, b;
+		cin >> a >> b;
+		cout << s.query(a, b).cnt << endl;
+	}
+	return 0;
+}
