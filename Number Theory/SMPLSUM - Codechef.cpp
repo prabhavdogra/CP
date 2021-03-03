bd295e3178b705f965eeeed37c5e71697a06e8ab
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-03-03 11:43:15 +0530

    Update SMPLSUM - Codechef.cpp

--- Number Theory/SMPLSUM - Codechef.cpp
+++ Number Theory/SMPLSUM - Codechef.cpp
@@ -55,11 +55,11 @@ void solve(ll n) {
 }
 
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	#ifndef ONLINE_JUDGE
-		// freopen("input.txt", "r", stdin) ;
-		// freopen("output.txt", "w", stdout) ;
-	#endif
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin) ;
+	freopen("output.txt", "w", stdout) ;
+    #endif
     MAXN = 1e7 + 5;
     sieve() ;
 	ll t;
