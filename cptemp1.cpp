36fb80655c50dcd8041da601c6c88c03bda5f945
Prabhav Dogra <55079966+prabhavdogra@users.noreply.github.com>
2021-11-03 06:58:54 +0530

    Update cptemp1.cpp

--- cptemp1.cpp
+++ cptemp1.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 #ifdef prabhav_
     // #define TERMINAL
     #include "Headers/debug.cpp"
@@ -6,10 +10,6 @@
     #pragma GCC optimize("O3")
     #pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,fma")
 #endif
-#include <bits/stdc++.h>
-using namespace std; 
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 
 #define int long long int
 #define vi vector<int>
@@ -25,6 +25,7 @@ using namespace __gnu_pbds;
 #define setbits(x) __builtin_popcount(x)
 #define setbitsll(x) __builtin_popcountll(x)
 #define MSB(n) (32 - __builtin_clz(n))
+#define isPresent(ds, el) (ds.find(el) != ds.end())
 #define pb push_back
 #define ff first
 #define ss second
@@ -32,15 +33,12 @@ constexpr int64_t INF = 2e18;
 constexpr int64_t M = 1e9 + 7;
 int ceil(int a, int b) { return (a + b - 1) / b; }
 
-
 void prabhav() {
-    int n, temp, ans = 0;
-    cin >> n ;
     
 }
 
 signed main() {
-    ios::sync_with_stdio(0), cin.tie(0);
+    cin.tie(nullptr)->sync_with_stdio(false);
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
